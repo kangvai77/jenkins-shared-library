bcad0dec7e178c2ecbcba4f838bd0fca352bd8db
kangvai <kangvai.cn@gmail.com>
2026-07-17 22:24:47 +0800

    change to declarative

--- vars/customPod.groovy
+++ vars/customPod.groovy
@@ -1,28 +1,18 @@
 // vars/customPod.groovy
-def call(String image, Closure body) {
-    podTemplate(
-        namespace: 'pipeline',
-        containers: [
-            containerTemplate(
-                name: 'jnlp',
-                image: 'crpi-whdz2l2sopzelm2i-vpc.cn-beijing.personal.cr.aliyuncs.com/kangvai/jenkins-inbound-agent:3383.vc8881d4b_0e76-1-jdk25',
-		resourceRequestCpu: '200m',
-                resourceRequestMemory: '256Mi',
-                resourceLimitCpu: '500m',
-                resourceLimitMemory: '512Mi'
-            ),
-            containerTemplate(
-                name: 'worker',
-                image: image,
-                command: 'cat',
-                ttyEnabled: true,
-		resourceRequestCpu: '100m',
-                resourceRequestMemory: '128Mi',
-                resourceLimitCpu: '500m',
-                resourceLimitMemory: '256Mi'
-            )
-        ]
-    ) {
-        body()
-    }
+def call(String image) {
+    return """
+apiVersion: v1
+kind: Pod
+metadata:
+  namespace: pipeline
+spec:
+  restartPolicy: Never
+  containers:
+  - name: jnlp
+    image: crpi-whdz2l2sopzelm2i-vpc.cn-beijing.personal.cr.aliyuncs.com/kangvai/jenkins-inbound-agent:3383.vc8881d4b_0e76-1-jdk25
+  - name: worker
+    image: ${image}
+    command: ['cat']
+    tty: true
+"""
 }
